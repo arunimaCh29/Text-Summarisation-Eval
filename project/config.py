7d8262d739c4406e303dd4bca231811673c62762
Arunima <arunimachaurasia@gmail.com>
2025-06-03 19:22:24 +0200

    deleting the key

--- project/config.py
+++ project/config.py
@@ -4,7 +4,7 @@ class Config:
     def __init__(self):
         self.model = "bart"  # or "t5"
         self.detoxify_model = 'original'
-        self.api_keys = {"perspective": "AIzaSyCe9upRXgqBr5JQe5OW2wcv7B3v8uTjdKw"}
+        self.api_keys = {"perspective": ""}
         self.dataset = 'reddit_tldr'  # multi_news or reddit_tldr
         self.perspective_url = f"https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key={self.api_keys['perspective']}"
 
